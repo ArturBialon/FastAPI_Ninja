63fa37ea00a2258f842132da7bbfbc53efc59067
Artur <4.Bialon@gmail.com>
2023-11-19 19:34:08 +0100

    login func

--- app/API/Controllers/user_controller.py
+++ app/API/Controllers/user_controller.py
@@ -1,19 +1,23 @@
 from datetime import timedelta
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from app.API.Authorization.auth import LoginForm
 from fastapi.security import OAuth2PasswordRequestForm
-from app.API.Authorization.auth import TOKEN_EXP_MIN, create_access_token, get_hashed_password, verify_password
+from app.API.Authorization.auth import TOKEN_EXP_MIN, LoginForm, create_access_token, get_hashed_password, verify_password
 from app.API.Models.system_user_table import AccessToken, SystemUser, GetSystemUserDTO
 from app.API.database import session_local
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get("/users")
 async def read_users():
     session = session_local()
     users = session.query(SystemUser.Name).all()
-    names = [user[0] for user in users]
+    names = [{user.Name, user.Password} for user in users]
     result = names
     if result is None:
         raise HTTPException(status_code=404, detail="Users not found")
@@ -24,34 +28,44 @@ async def read_users():
 @router.get("/users/{user_id}")
 async def read_users(user_id: int):
     session = session_local()
-    
+
     user = session.query(SystemUser).where(SystemUser.Id == user_id).first()
     result: GetSystemUserDTO = {
-        user.Id, 
-        user.Name, 
+        user.Id,
+        user.Name,
         user.Role
-        }
-    
+    }
+
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     session.close()
     return result
 
-@router.post("/users/token", response_model=AccessToken)
-async def login_for_access(form_data: OAuth2PasswordRequestForm=Depends()):
+
+@router.post("/login", response_model=None)
+async def login_for_access(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+    encoded_jwt = ""
+    form = LoginForm()
+    error_message = ""
+    
     session = session_local()
-    user = session.query(SystemUser).where(SystemUser.Name == form_data.username).first()
+    user = session.query(SystemUser).where(
+        SystemUser.Name == form_data.username).first()
     session.close()
     if not user:
-        return False
-    if not verify_password(form_data.password, user.Password):
-        raise HTTPException(status_code=401,
-                             detail="Icorrect username or password",
-                             headers={"WWW-Authenticate": "Bearer"})
-    access_token_expires = timedelta(minutes=TOKEN_EXP_MIN)
-    access_token = create_access_token(
-        data={"sub": user.Name}, expires_delta=access_token_expires
-    )
-    
-    return {"access_token": access_token, "token_type": "bearer"}
+        error_message = "Incorrect username or password"
+    elif not verify_password(form_data.password, user.Password):
+        error_message = "Incorrect username or password"
+    else:
+        access_token_expires = timedelta(minutes=TOKEN_EXP_MIN)
+        encoded_jwt = create_access_token(
+            data={"sub": user.Name}, expires_delta=access_token_expires
+        )
+    return templates.TemplateResponse("question.html", {"request": request, "encoded_jwt": encoded_jwt, "form": form, "error_message": error_message})
 
+
+@router.get("/", response_class=HTMLResponse)
+async def show_form(request: Request):
+    encoded_jwt = ""
+    form = LoginForm()
+    return templates.TemplateResponse("question.html", {"request": request, "encoded_jwt": encoded_jwt, "form": form})
--- main.py
+++ main.py
@@ -1,43 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from app.API.Authorization.auth import LoginForm
 from app.API.Controllers import user_controller
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(user_controller.router)
-    
-@app.get("/about")
-def about():
-    return "All you need to know about DPR"
-
-templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
-async def show_form(request: Request):
-    form = LoginForm()
-    return templates.TemplateResponse("question.html", {"request": request, "form": form})
 
-@app.get("/login", response_class=HTMLResponse)
-async def login(request: Request):
-    form = LoginForm()
-    return templates.TemplateResponse("login.html", {"request": request, "form": form})
-
-@app.post("/login")
-async def process_login(request: Request, form: LoginForm):
-    # Perform login logic here
-    # Redirect or return a response based on the login result
-    return {"message": "Login successful"}
-
-    #     <hr>
-    #             <div>
-    #                 {% if dpr.published == true %}
-    #                     <p>{{ dpr.response }}</p>
-    #                     
-    #                 {% else %}
-    #                     <p>Waiting for result.</p>
-    #                {% endif %}
-    #            </div>
